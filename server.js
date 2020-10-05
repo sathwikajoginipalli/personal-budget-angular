084e7489e16b8f293f13dcfe6a5d5f1a0b0f5d19
Sathwika Joginipalli <sathwikajoginipalli@gmail.com>
2020-10-05 11:04:02 -0400

    added cors

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
+//Budget API
+const cors = require('cors');
 const express = require('express');
 const app = express()
 const port = 3000;
 var budget = require('./budget.json');
 
-app.use('/', express.static('public'));
+app.use(cors());
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
 app.get('/budget', (req, res) => {
     res.json(budget);
 });
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}');
+    console.log('API listening at http://localhost:${port}');
 });
