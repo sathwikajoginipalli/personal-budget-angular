8eb5576e0afb8ea58a795457a639f62e78a7ca26
Sathwika Joginipalli <sathwikajoginipalli@gmail.com>
2020-10-08 22:07:33 -0400

    changed d3 angular

--- personal-budget/src/app/homepage/homepage.component.ts
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -1,13 +1,20 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Chart } from 'chart.js';
+import { DataService } from '../data.service';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'pb-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements AfterViewInit {
+export class HomepageComponent implements AfterViewInit, OnInit {
+  // tslint:disable-next-line: variable-name
+  private _current: any;
+
+
+  constructor(private http: HttpClient, public data1: DataService) { }
 
   public dataSource =  {
     datasets: [
@@ -28,10 +35,50 @@ export class HomepageComponent implements AfterViewInit {
     labels: []
   };
 
-  constructor(private http: HttpClient) { }
+  public result = [];
+  public width = 250 ;
+  public height = 250 ;
+  public radius = Math.min(this.width, this.height) / 2;
+
+  public svg = d3.select('#chart').append('svg').append('g').attr('class', 'slices')
+  .attr('class', 'labels').attr('class', 'lines').attr('transform', 'translate( ' + this.width / 2 + ',' + this.height / 2 + ')');
+
+  // svg.append('g').attr('class', 'slices');
+  // svg.append('g').attr('class', 'labels');
+  // svg.append('g').attr('class', 'lines');
+
+  public pie = d3.pie()
+        .sort(null)
+        .value((d: any) => {
+          return d.value;
+        });
+
+  public arc = d3.arc()
+        .outerRadius(this.radius * 0.8)
+        .innerRadius(this.radius * 0.4);
+
+  public outerArc = d3.arc()
+        .innerRadius(this.radius * 0.9)
+        .outerRadius(this.radius * 0.9);
+
+  public color = d3.scaleOrdinal()
+        .range([
+          '#98abc5',
+          '#8a89a6',
+          '#7b6888',
+          '#6b486b',
+          '#a05d56',
+          '#d0743c',
+          '#ff8c00',
+          '#36a2eb'
+        ]);
+
+  public key = (d) => {
+    return d.data.label;
+  }
 
   ngAfterViewInit(): void {
-    this.http.get('http://localhost:3000/budget')
+    this.data1.Budget()
     .subscribe((res: any) => {
       console.log(res);
       for (let i = 0; i < res.length; i++) {
@@ -53,4 +100,128 @@ export class HomepageComponent implements AfterViewInit {
     });
   }
 
+
+  private randomData(): void {
+    this.data1.Budget()
+    .subscribe((res: any) => {
+      for (let i = 0; i < res.length; i++) {
+        this.result[i] = res[i].title;
+      }
+      console.log(this.result);
+      return this.result.map(label => {
+        // tslint:disable-next-line: object-literal-shorthand
+        return { label: this.result , value: Math.random() };
+      });
+    });
+  }
+  private change(data): void {
+
+    const slice = this.svg
+      .select('.slices')
+      .selectAll('path.slice')
+      .data(this.pie(data), this.key);
+
+    slice
+      .enter()
+      .insert('path')
+      .style('fill', (d): any => {
+        return this.color(d.data.toString());
+      })
+      .attr('class', 'slice');
+
+    slice
+      .transition()
+      .duration(1000)
+      .attrTween('d', (d) => {
+        this._current = this._current || d;
+        const interpolate = d3.interpolate(this._current, d);
+        this._current = interpolate(0);
+        return (t) => {
+          return this.arc(interpolate(t));
+        };
+      });
+
+    slice.exit().remove();
+
+    /* ------- TEXT LABELS -------*/
+
+    const text = this.svg.select('.labels').selectAll('text').data(this.pie(data), this.key);
+
+    text
+      .enter()
+      .append('text')
+      .attr('dy', '.35em')
+      .text((d) => {
+        return d.data.label;
+      });
+
+    // tslint:disable-next-line: typedef
+    function midAngle(d: any) {
+      return d.startAngle + (d.endAngle - d.startAngle) / 2;
+    }
+
+    text
+      .transition()
+      .duration(1000)
+      .attrTween('transform', (d) => {
+        this._current = this._current || d;
+        const interpolate = d3.interpolate(this._current, d);
+        this._current = interpolate(0);
+        return (t: any) => {
+          const d2 = interpolate(t);
+          const pos = this.outerArc.centroid(d2);
+          pos[0] = this.radius * (midAngle(d2) < Math.PI ? 1 : -1);
+          return 'translate(' + pos + ')';
+        };
+      })
+      .styleTween('text-anchor', (d) => {
+        this._current = this._current || d;
+        const interpolate = d3.interpolate(this._current, d);
+        this._current = interpolate(0);
+        return (t: any) => {
+          const d2 = interpolate(t);
+          return midAngle(d2) < Math.PI ? 'start' : 'end';
+        };
+      });
+
+    text.exit().remove();
+
+    const polyline = this.svg
+      .select('.lines')
+      .selectAll('polyline')
+      .data(this.pie(data), this.key);
+
+    polyline.enter().append('polyline');
+
+    polyline.transition()
+      .duration(1000)
+      .attrTween('points', (d): any => {
+        this._current = this._current || d;
+        const interpolate = d3.interpolate(this._current, d);
+        this._current = interpolate(0);
+        return (t) => {
+          const d2 = interpolate(t);
+          const pos = this.outerArc.centroid(d2);
+          pos[0] = this.radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+          return [this.arc.centroid(d2), this.outerArc.centroid(d2), pos];
+        };
+      });
+
+    polyline.exit().remove();
+  }
+
+  ngOnInit(): void {
+
+    setTimeout(() => {
+      this.change(this.randomData());
+    }, 1000);
+
+    d3.select(window).on('load', () => {
+      this.change(this.randomData());
+    });
+    d3.select('.randomize').on('click', () => {
+      this.change(this.randomData());
+    });
+
+}
 }
